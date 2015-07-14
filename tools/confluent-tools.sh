42d86bc04675f9f1f4f6121b119faace7be9361e
Stuart Wong <cgs.wong@gmail.com>
2015-07-14 12:43:20 -0500

    Add better support for remote ZK, Kafka, and SR.

--- tools/confluent-tools.sh
+++ tools/confluent-tools.sh
@@ -3,10 +3,18 @@
 # For commonly used command-line tools, this entrypoint script adds the
 # necessary parameters to point them at the linked containers.
 
+: ${TOOLS_ZK_CONNECTION_URL:=$ZOOKEEPER_PORT_2181_TCP_ADDR:$ZOOKEEPER_PORT_2181_TCP_PORT}
+: ${TOOLS_KAFKA_CONNECTION_URL:=$KAFKA_PORT_9092_TCP_ADDR:$KAFKA_PORT_9092_TCP_PORT}
+: ${TOOLS_SR_CONNECTION_URL:=$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR:$SCHEMA_REGISTRY_PORT_8081_TCP_PORT}
+
+export TOOLS_ZK_CONNECTION_URL
+export TOOLS_KAFKA_CONNECTION_URL
+export TOOLS_SR_CONNECTION_URL
+
 if [ "$1" = 'kafka-avro-console-consumer' ]; then
     shift
     exec /usr/bin/kafka-avro-console-consumer \
-        --zookeeper ${ZOOKEEPER_PORT_2181_TCP_ADDR}:${ZOOKEEPER_PORT_2181_TCP_PORT} \
+        --zookeeper ${TOOLS_ZK_CONNECTION_URL} \
         --property schema.registry.url=http://${SCHEMA_REGISTRY_PORT_8081_TCP_ADDR}:${SCHEMA_REGISTRY_PORT_8081_TCP_PORT} \
         "$@"
 fi
@@ -14,8 +22,8 @@ fi
 if [ "$1" = 'kafka-avro-console-producer' ]; then
     shift
     exec /usr/bin/kafka-avro-console-producer \
-        --broker-list ${KAFKA_PORT_9092_TCP_ADDR}:${KAFKA_PORT_9092_TCP_PORT} \
-        --property schema.registry.url=http://${SCHEMA_REGISTRY_PORT_8081_TCP_ADDR}:${SCHEMA_REGISTRY_PORT_8081_TCP_PORT} \
+        --broker-list ${TOOLS_KAFKA_CONNECTION_URL} \
+        --property schema.registry.url=http://${TOOLS_SR_CONNECTION_URL} \
         "$@"
 fi
 

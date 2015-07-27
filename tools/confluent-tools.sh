47f88c27196361377569ed538303bc727f012892
Stuart Wong <cgs.wong@gmail.com>
2015-07-27 09:50:34 -0500

    Make TOOLS connection string naming clearer.

--- tools/confluent-tools.sh
+++ tools/confluent-tools.sh
@@ -3,27 +3,27 @@
 # For commonly used command-line tools, this entrypoint script adds the
 # necessary parameters to point them at the linked containers.
 
-: ${TOOLS_ZK_CONNECTION_URL:=$ZOOKEEPER_PORT_2181_TCP_ADDR:$ZOOKEEPER_PORT_2181_TCP_PORT}
-: ${TOOLS_KAFKA_CONNECTION_URL:=$KAFKA_PORT_9092_TCP_ADDR:$KAFKA_PORT_9092_TCP_PORT}
-: ${TOOLS_SR_CONNECTION_URL:=$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR:$SCHEMA_REGISTRY_PORT_8081_TCP_PORT}
+: ${TOOLS_ZK_CONNECTION_STR:=$ZOOKEEPER_PORT_2181_TCP_ADDR:$ZOOKEEPER_PORT_2181_TCP_PORT}
+: ${TOOLS_KAFKA_CONNECTION_STR:=$KAFKA_PORT_9092_TCP_ADDR:$KAFKA_PORT_9092_TCP_PORT}
+: ${TOOLS_SR_CONNECTION_STR:=$SCHEMA_REGISTRY_PORT_8081_TCP_ADDR:$SCHEMA_REGISTRY_PORT_8081_TCP_PORT}
 
-export TOOLS_ZK_CONNECTION_URL
-export TOOLS_KAFKA_CONNECTION_URL
-export TOOLS_SR_CONNECTION_URL
+export TOOLS_ZK_CONNECTION_STR
+export TOOLS_KAFKA_CONNECTION_STR
+export TOOLS_SR_CONNECTION_STR
 
 if [ "$1" = 'kafka-avro-console-consumer' ]; then
     shift
     exec /usr/bin/kafka-avro-console-consumer \
-        --zookeeper ${TOOLS_ZK_CONNECTION_URL} \
-        --property schema.registry.url=http://${SCHEMA_REGISTRY_PORT_8081_TCP_ADDR}:${SCHEMA_REGISTRY_PORT_8081_TCP_PORT} \
+        --zookeeper ${TOOLS_ZK_CONNECTION_STR} \
+        --property schema.registry.url=http://${TOOLS_SR_CONNECTION_STR} \
         "$@"
 fi
 
 if [ "$1" = 'kafka-avro-console-producer' ]; then
     shift
     exec /usr/bin/kafka-avro-console-producer \
-        --broker-list ${TOOLS_KAFKA_CONNECTION_URL} \
-        --property schema.registry.url=http://${TOOLS_SR_CONNECTION_URL} \
+        --broker-list ${TOOLS_KAFKA_CONNECTION_STR} \
+        --property schema.registry.url=http://${TOOLS_SR_CONNECTION_STR} \
         "$@"
 fi
 

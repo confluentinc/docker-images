1d692fe4ee52231a47b3f758dc2f49a2dbbc9589
Jeremy Custenborder <jcustenborder@gmail.com>
2016-03-25 16:49:32 -0700

    Change to always print the amount of memory for the container.

--- kafka/kafka-docker.sh
+++ kafka/kafka-docker.sh
@@ -40,10 +40,10 @@ export KAFKA_ZOOKEEPER_CONNECTION_TIMEOUT_MS
 export KAFKA_AUTO_CREATE_TOPICS_ENABLE
 export KAFKA_DELETE_TOPIC_ENABLE
 
+TOTAL_MEMORY=`grep MemTotal /proc/meminfo | awk '{print $2}'`
+echo "Container was launched with ${TOTAL_MEMORY}k of memory"
 
-if [ -z "$KAFKA_HEAP_OPTS" ]; then
-  TOTAL_MEMORY=`grep MemTotal /proc/meminfo | awk '{print $2}'`
-  echo "Container was launched with ${TOTAL_MEMORY}k of memory"
+if [ -z ${KAFKA_HEAP_OPTS+x} ]; then
   HEAP_SIZE=`awk "BEGIN { rounded = sprintf('%.0f', ${TOTAL_MEMORY} * 0.80); print rounded }"`
   KAFKA_HEAP_OPTS="-Xmx${HEAP_SIZE}K"
   echo "Set KAFKA_HEAP_OPTS to ${KAFKA_HEAP_OPTS}"
